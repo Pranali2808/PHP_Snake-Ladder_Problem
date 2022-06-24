3f7d90c9fc1d1ca3c08ddf35aa8ef9d7779c6850
pranali2808 <ranepranali1997@gmail.com>
2022-06-24 21:12:02 +0530

    roll the dice to get random numbers

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -1,13 +1,39 @@
 <?php
-//UC1:player starting position is zero.
+/*UC2:starting position is zero.
+Roll the dice to getting random numbers
+*/
 class SnakeLadder{
     private $startposition = 0;
+    private $previousposition;
+    private $count = 0;
 
-   public function welcome()
+    public function welcome()
     {
-        echo "Welcome to the Snake and Ladder Game";
+        echo "Welcome to the Snake and Ladder Game \n";
+    }
+
+   public function RollDice()
+    {
+        if($this->previousposition==$this->startposition)
+        {
+         echo "game started \n";
+         //break;
+     }
+        $DiceNum = rand (1,6);//generate random number between  1 to 6.
+        $this->count ++ ;
+        echo "The Dice number is : $DiceNum \n ";
+        echo "You rolled dice $this->count times \n";
+       
+       $this->startposition =$this->startposition + $DiceNum ;
+       echo "New position is : $this->startposition \n";
+       
+
     }
 }
 
-$game = new SnakeLadder();
-$game->welcome();
+$game = new SnakeLadder();//create object
+$game->welcome();//calling the function
+$game->RollDice();
+
+
+?>
